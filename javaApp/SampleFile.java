49df32456b6c51025c4ab3fbad664c264cd34228
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-07-13 13:05:15 +0530

    Uploaded JavaApp for testing in Docker

---
+++ javaApp/SampleFile.java
@@ -0,0 +1,6 @@
+public class SampleFile{
+    public static void main(String[] args) {
+        System.out.println("Sample Example for Java Application");
+        System.out.println("Sample test code for Java App using Docker");
+    }
+}
